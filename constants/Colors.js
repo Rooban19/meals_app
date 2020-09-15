28d3afffac4795ad8afc12a9e978a0f42a5453dc
Rooban <roobandc8@gmail.com>
2020-09-15 11:51:00 +0530

    header with navigation options and sets the colors in the constant folder

---
+++ constants/Colors.js
@@ -0,0 +1,4 @@
+export default Colors = {
+    primaryColor: '#4a148c',
+    accentColor: '#ff6f00',
+};
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,31 +1,45 @@
-import React from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
+import React from 'react';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
-const renderGridItem = itemdata => {
-  return (
-    <View style={styles.gridItem}>
-      <Text>{itemdata.item.title}</Text>
-    </View>
-  );
-};
 const CategoriesScreen = props => {
-  return (
-    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
-  );
+	const renderGridItem = itemdata => {
+		return (
+			<TouchableOpacity
+				style={styles.gridItem}
+				onPress={() => {
+					props.navigation.navigate({ routeName: 'CategoryMeals' });
+				}}
+			>
+				<View>
+					<Text>{itemdata.item.title}</Text>
+				</View>
+			</TouchableOpacity>
+		);
+	};
+	return <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />;
 };
+
+CategoriesScreen.navigationOptions = {
+	headerTitle: 'Meal Categories',
+	headerStyle: {
+		backgroundColor: Colors.primaryColor,
+	},
+};
+
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  gridItem: {
-    flex: 1,
-    margin: 16,
-    height: 150,
-  },
+	screen: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	gridItem: {
+		flex: 1,
+		margin: 16,
+		height: 150,
+	},
 });
 
 export default CategoriesScreen;
